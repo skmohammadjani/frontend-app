6d3ac7c11bbebe59eb266a88cd5c294c3c697aba
Manu K M <manukm171995@gmail.com>
2023-01-24 13:02:40 +0530

    Initial Folder Setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <h2>Hello</h2>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
